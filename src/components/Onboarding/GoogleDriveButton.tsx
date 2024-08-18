f5fd7492193dc6d150e28ed2d861a8e73b29c895
Eesha Moona <eeshamoona@gmail.com>
2024-08-18 16:21:39 -0500

    trying out the google drive picker

--- src/components/Onboarding/GoogleDriveButton.tsx
+++ src/components/Onboarding/GoogleDriveButton.tsx
@@ -1,25 +1,43 @@
-import { useRecoilValue } from "recoil";
-import { userAtom } from "@/atoms/userAtom";
+import useDrivePicker from "react-google-drive-picker";
 import { Button } from "@chakra-ui/react";
+import { userAtom } from "@/atoms/userAtom";
+import { useRecoilState } from "recoil";
+import { useEffect } from "react";
+import { authModalState } from "@/atoms/authModalAtom";
 
-function GoogleDriveButton() {
-  const user = useRecoilValue(userAtom);
+const GoogleDriveButton = () => {
+  const [openPicker] = useDrivePicker();
+  const [userState] = useRecoilState(userAtom);
+  const [, setAuthModalState] = useRecoilState(authModalState);
 
-  const fetchDriveFiles = async () => {
-    if (user.user && user.user?.googleAuthToken) {
-      const response = await fetch(
-        "https://www.googleapis.com/drive/v3/files",
-        {
-          headers: {
-            Authorization: `Bearer ${user.user.googleAuthToken}`,
-          },
-        }
-      );
-      const data = await response.json();
-      console.log(data);
-    } else {
-      console.error("No Google Auth Token found");
+  useEffect(() => {
+    if (!userState.user) {
+      console.error("User is not signed in.");
+      setAuthModalState({ isOpen: true, mode: "login" });
     }
+    if (!userState.user?.googleAuthToken) {
+      console.error("User has not authenticated with Google.");
+      setAuthModalState({ isOpen: true, mode: "login" });
+    }
+  }, [setAuthModalState, userState.user]);
+
+  const handleOpenPicker = () => {
+    openPicker({
+      clientId: "xxxxxxxxxxxxxxxxx",
+      developerKey: "xxxxxxxxxxxx",
+      viewId: "DOCS",
+      token: userState.user?.googleAuthToken ?? "",
+      showUploadView: true,
+      showUploadFolders: true,
+      supportDrives: true,
+      multiselect: true,
+      callbackFunction: (data) => {
+        if (data.action === "cancel") {
+          console.log("User clicked cancel/close button");
+        }
+        console.log(data);
+      },
+    });
   };
 
   return (
@@ -27,11 +45,11 @@ function GoogleDriveButton() {
       variant="solid"
       w="full"
       loadingText="Processing..."
-      onClick={fetchDriveFiles}
+      onClick={handleOpenPicker}
     >
       Upload Google Takeout Data
     </Button>
   );
-}
+};
 
 export default GoogleDriveButton;
