89031b5f10b8f78a1392e22f87d14f4d203cb5ec
Eesha Moona <eeshamoona@gmail.com>
2024-08-18 14:29:10 -0500

    updating debug logs

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "@/utils/hooks/useAuth";
 import { Box, Spinner, Text, Alert, AlertIcon, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
 export default function GoogleSignIn() {
   const [status, setStatus] = useState("pending");
   const { user, loading, googleSignIn, logOut } = useAuth();
+  const router = useRouter();
 
   useEffect(() => {
+    console.log("GoogleSignIn useEffect triggered");
     const handleGoogleSignIn = async () => {
       try {
         await googleSignIn().then(() => {
+          console.log("Google sign-in successful");
+          router.push("/");
           window.close();
           setStatus("success");
         });
@@ -19,7 +24,7 @@ export default function GoogleSignIn() {
       }
     };
     handleGoogleSignIn();
-  }, [googleSignIn]);
+  }, []);
 
   return (
     <Box textAlign="center" p={5}>
--- src/utils/hooks/useAuth.ts
+++ src/utils/hooks/useAuth.ts
@@ -69,7 +69,6 @@ export function useAuth() {
     try {
       console.log("Signing in with Google using redirect");
       await signInWithRedirect(auth, provider);
-      window.close();
       console.log("Sign in with Google using redirect successful");
     } catch (error) {
       console.error("Error signing in with Google using redirect: ", error);
